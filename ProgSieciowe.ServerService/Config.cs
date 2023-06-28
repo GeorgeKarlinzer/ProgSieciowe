68fd478039f359ec36b28944d2c0d847890812a4
GeorgeKarlinzer <georg11.01@mail.ru>
2023-06-28 19:10:24 +0200

    remove service

--- ProgSieciowe.ServerService/Config.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProgSieciowe.ServerService
-{
-    public class Config
-    {
-        public string IpAddress { get; set; }
-        public string Port { get; set; }
-        public string WorkingDirectory { get; set; }
-    }
-}
